ad2e77f57ef6f41955926cc11bb1473074bc201b
DonnyAndre29 <donandre43@yahoo.com>
2024-03-29 20:45:23 -0700

    path heroku

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 
 // Serve the index.html file
-router.get('/', (req, res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
  
 });
